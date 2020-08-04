b13470a7ed53dd0088e7d6315f6c88630f80dd4d
Kathleen McKiernan <kathleenmckiernan@UofNs-iMac-3.local>
2020-08-03 20:29:17 -0400

    fix indentation

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create file in /tmp using Puppet
 
 file { '/tmp/holberton':
-  path => '/tmp/holberton',
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
